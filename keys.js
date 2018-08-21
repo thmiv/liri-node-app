ff216190da47e4b1c4d60af530f48060ec7dda22
thmiv <thmiv4@gmail.com>
2018-08-21 01:31:06 -0500

    concert search functioning

--- keys.js
+++ keys.js
@@ -1,6 +1,10 @@
-console.log('this is loaded');
+//console.log('Keys is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
+
+exports.bandsintown = {
+  key: process.env.BIT_API_KEY
 };
--- liri.js
+++ liri.js
@@ -1,15 +1,65 @@
 require("dotenv").config();
+var request = require('request');
+var chalk = require("chalk");
+var moment = require("moment");
 var apiKeys = require("./keys.js");
-console.log(apiKeys);
 
-var liriQuery = process.argv[2];
+//console.log(apiKeys);
+var bandsKey = apiKeys.bandsintown.key;
+//console.log(bandsKey);
+
+var nodeArgs = process.argv;
+var liriQuery = nodeArgs[2];
 var searchItem = "";
 
-if (process.argv[3] != undefined) {
-    for (i = 3; i < process.argv.length; i++) {
-        searchItem += process.argv[i] + " ";
+if (nodeArgs[3] != undefined) {
+    for (i = 3; i < nodeArgs.length; i++) {
+        searchItem += nodeArgs[i] + " ";
     }
+    searchItem = searchItem.trim();
 }
 
-console.log(liriQuery);
-console.log(searchItem);
+//console.log(liriQuery);
+//console.log(searchItem);
+
+switch (liriQuery) {
+    case 'concert-this':
+        searchBandsInTown(searchItem);
+        break;
+    case 'spotify-this-song':
+        console.log("spotify ths");
+        break;
+    case 'movie-this':
+        console.log("movie this");
+        break;
+    case 'do-what-it-says':
+        console.log("do this");
+        break
+    default: console.log("Please enter valid search command.");
+}
+
+function searchBandsInTown(artist) {
+    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + bandsKey;
+    //console.log(queryURL);
+    request(queryURL, function (error, response, body) {
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+            // Printing the entire object to console
+            var eventInfo = JSON.parse(body);
+            //console.log(eventInfo.length);
+            // Constructing HTML containing the artist information
+            for (i = 0; i < 3 && i < eventInfo.length; i++) {
+                var eventDate = moment(eventInfo[i].datetime).format("MM[/]DD[/]YY, h:mm a");
+                var eventVenue = eventInfo[i].venue;
+                // Empty the contents of the response
+                console.log(chalk.yellow(artist) + " will be playing at " +
+                    chalk.cyan(eventVenue.name) + " in " +
+                    chalk.magenta(eventVenue.city) + ", " +
+                    chalk.magenta(eventVenue.region) + " " +
+                    chalk.magenta(eventVenue.country),
+                    " on " + eventDate);
+            }
+        }
+    });
+}
